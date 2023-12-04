780cf18a6c188c049db212baf1ca46a76b0edaac
PAVAN-CHANDRA-ALE <120574962+PAVAN-CHANDRA-ALE@users.noreply.github.com>
2023-12-04 17:26:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ function toggleMenu(){
     // console.log("Hi Pavan");
     if(menuLinks.style.display == 'none')
     {
-        menuLinks.style.display='block';
         menuIcon.innerHTML = '<img src="./images/cross img.png" height="40px" alt="Hi">';
+        menuLinks.style.display='block';
     }else{
         menuLinks.style.display='none';
         menuIcon.innerHTML = '<i class="ri-menu-3-fill">';
